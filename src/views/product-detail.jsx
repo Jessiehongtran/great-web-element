4c18f7114fea96e973f5ac70f0de0d8a3feb4c8f
htran2@babson.edu <hongtran@Hongs-MacBook-Pro.local>
2022-04-26 16:59:23 -0700

    got the product detail component done

--- src/views/product-detail.jsx
+++ src/views/product-detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/product-detail.css'
+import '../styles/product-detail.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class ProductDetail extends React.Component {
     constructor(){
@@ -14,56 +15,56 @@ export default class ProductDetail extends React.Component {
             <div className="detail-container">
                 <div className="detail-wrapper">
                     <div className="detail-nav">
-                        <div className="flex">
-                            <p>Shop</p>
-                            <p>Collections</p>
-                            <p>Our Story</p>
+                        <div className="flex brown">
+                            <p className="flex-item">Shop</p>
+                            <p className="flex-item">Collections</p>
+                            <p className="flex-item">Our Story</p>
                         </div>
-                        <div>
+                        <div className="product-name">
                             <p>MANDRAGORA</p>
                         </div>
                         <div className="flex">
-                            <div>
-                                <p>Ukraine</p>
-                                <div>Icon</div>
+                            <div className="flex flex-item">
+                                <p className="brown">Ukraine</p>
+                                <FontAwesomeIcon className="icon" icon={["fa", "fa-angle-down"]} />
                             </div>
-                            <div>
-                                <p>Cart</p>
-                                <div>Icon</div>
+                            <div className="flex flex-item">
+                                <p className="brown">Cart</p>
+                                <div className="icon cart-icon">2</div>
                             </div>
                         </div>
                     </div>
                     <div className="product-info">
-                        <div class="product-image">
-                            <img src="" alt="product-image" />
+                        <div className="image">
+                            <img className="shampoo" src="https://pngimg.com/uploads/shampoo/shampoo_PNG12.png" alt="product-image" />
                         </div>
-                        <div class="detail">
-                            <p>FACIAL CLEANSER</p>
-                            <div>
-                                <p>Collections</p>
-                                <p>Organic Skin</p>
+                        <div className="detail">
+                            <p className="product-category">FACIAL CLEANSER</p>
+                            <div className="flex minus-top">
+                                <p className="text-title brown">Collections: </p>
+                                <p className="collection item">Organic Skin</p>
                             </div>
-                            <p>For the brand visual style, we've researched some 15-16th century botanical books to create a series of illustrations that capture spirit of medieval era in a contemporary style.</p>
-                            <div class="volume-options">
-                                <button>40 ml</button>
-                                <button>80 ml</button>
-                                <button>120 ml</button>
+                            <p className="brown product-intro">For the brand visual style, we've researched some 15-16th century botanical books to create a series of illustrations that capture spirit of medieval era in a contemporary style.</p>
+                            <div className="volume-options">
+                                <button className="volume-option active pointer">40 ml</button>
+                                <button className="volume-option pointer">80 ml</button>
+                                <button className="volume-option pointer">120 ml</button>
                             </div>
-                            <div class="">
-                                <p>Organic Skin Organic</p>
-                                <div>
-                                    <button>-</button>
-                                    <p>1</p>
-                                    <button>+</button>
+                            <div class="flex top-bottom-gap">
+                                <p className="text-title brown">Organic Skin Organic</p>
+                                <div class="flex buy-action">
+                                    <button class="math-btn pointer">-</button>
+                                    <span className="buy-count">1</span>
+                                    <button class="math-btn pointer">+</button>
                                 </div>
                             </div>
-                            <div>
-                                <p>ADD TO BAG</p>
-                                <p>$48 USD</p>
+                            <div className="flex summary space-between">
+                                <span>ADD TO BAG</span>
+                                <span className="total-cost">$48 USD</span>
                             </div>
-                            <p>***Apply a small amount to damp skin. Massage in circular motions before rinsing with water.</p>
-                            <div>
-                                <img src="" alt="stamp"/>
+                            <p className="note">***Apply a small amount to damp skin. Massage in circular motions before rinsing with water.</p>
+                            <div className="stamp">
+                                <img className="stamp-img" src="http://bsrservices.com/wp-content/uploads/2021/04/Eco-Friendly-BSR-1.png" alt="stamp"/>
                             </div>
                         </div>
                     </div>
