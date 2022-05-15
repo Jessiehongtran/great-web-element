90279b92524d88078e8c8f3e28787175ebec9353
htran2@babson.edu <hongtran@Hongs-MacBook-Pro.local>
2022-05-15 09:15:11 -0700

    added star rating

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 export default class Review extends React.Component {
@@ -32,10 +34,18 @@ export default class Review extends React.Component {
         //     this.setState({x: this.state.x - 10})
         //     console.log(this.state.x)
         // }.bind(this), 500)
+        const fullStarNum = Math.floor(review.rating)
+        const fullStars = Array.apply(0, Array(fullStarNum));
+        const halfStars = Array.apply(0, Array(5 - fullStarNum))
+
+        console.log(fullStars)
        
         return (
             <div style={{ margin: '10px', padding: '30px', width: `${cardWidth}px`, borderRadius: '12px', position: 'absolute', left: `${(review.id-1)*cardWidth*1.2+40+ this.state.x}px`, top: '0px', border: '1px solid black' }}>
-                <div>Stars</div>
+                <div className="stars">
+                    {fullStars.map(star => <FontAwesomeIcon icon={faStar} style={{color: 'orange'}}/>)}
+                    {halfStars.map(star => <FontAwesomeIcon icon={faStarHalf} style={{color: 'orange'}} />)}
+                </div>
                 <h1>{review.title}</h1>
                 <p style={{width: '100%'}}>{review.content}</p>
                 <div class="small-details" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
