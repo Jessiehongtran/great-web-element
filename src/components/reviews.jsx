78a42690b7e10aae009e783dc5a14eb5c3f42f37
Your Name <you@example.com>
2022-07-11 08:41:19 -0500

    Interesting moving left

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -5,13 +5,37 @@ import Review from '../components/review';
 export default class Reviews extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            x: 0,
+            y: 0
+        }
+        this.moveLeft = this.moveLeft.bind(this);
+    }
 
+    componentDidMount(){
+        this.moveLeft()
+    }
+
+    moveLeft() {
+        console.log(this.state.x)
+        if (this.state.x > -100) {
+            this.setState({x: this.state.x - 1})
+        } else {
+            this.setState({x: 100})
+        }
+
+        setTimeout(this.moveLeft, 80);
     }
 
     render(){
+
+        const { x, y } = this.state;
+
         return (
-            <div style={{display: 'flex' }}>
+            <div style={{  }}>
+                <div style={{ display: 'flex', position: 'absolute', left: `${x}%`, top: `${y}%`, transition: 'all 1s ease-in-out' }}>
                 {reviews.map(eachReview => <Review review={eachReview} />)}
+                </div>
             </div>
         )
     }
