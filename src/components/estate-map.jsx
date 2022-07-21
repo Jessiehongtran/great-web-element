7d55b9ba798446b010400a214b7b66fe6b0473a2
Your Name <you@example.com>
2022-07-21 13:31:00 -0700

    Got location based on IP address

--- src/components/estate-map.jsx
+++ src/components/estate-map.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaHRyYW4yMTE5NCIsImEiOiJjbDV1MnIwMGwwYndsM2lwZDE5bTVpdmZqIn0.RjMVBRVfulmVRT-gWXnfMQ';
 
@@ -11,12 +14,17 @@ export default class EstateMap extends React.Component {
         this.state = {
             lng: -122.4194,
             lat:  37.7749,
-            zoom: 9
+            zoom: 9,
+            city: '',
+            state: '',
+            country: ''
         };
         this.mapContainer = React.createRef();
+        this.getGeoInfo = this.getGeoInfo.bind(this);
+        this.getMapBox = this.getMapBox.bind(this);
     }
 
-    componentDidMount() {
+    getMapBox = () => {
         const { lng, lat, zoom } = this.state;
         const map = new mapboxgl.Map({
             container: this.mapContainer.current,
@@ -26,11 +34,39 @@ export default class EstateMap extends React.Component {
         })
     }
 
+    getGeoInfo = () => {
+        axios.get('https://api.ipregistry.co/?key=tryout')
+             .then(response => {
+                let data = response.data;
+                this.setState({
+                    city: data.location.city,
+                    state: data.location.region.name,
+                    country: data.location.country.name
+                })
+             })
+             .catch(err => {
+                console.log(err)
+             })
+    }
+
+    componentDidMount() {
+        this.getMapBox()
+        this.getGeoInfo()
+    }
+
     render () {
+
+        const { city, state, country } = this.state;
 
         return (
             <div>
-                <h2>Find the nearest of you</h2>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <h2 style={{ marginRight: '10px' }}>Find the nearest of you</h2>
+                    <div style={{ marginBottom: '-5px', color: '#097FDE' }}>
+                        <FontAwesomeIcon icon={faMapMarker} style={{ marginRight: '5px' }}/>
+                        <span>{city}, {state}, {country}</span>
+                    </div>
+                </div>
                 <div ref={this.mapContainer} className="map-container" style={{ height: '400px'}} />
             </div>
         )
