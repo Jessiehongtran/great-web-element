4283a6da719a0ca354a6630c35cea992a6297004
Your Name <you@example.com>
2022-07-22 08:37:23 -0700

    Made showing more houses function

--- src/components/estate-popular-houses.jsx
+++ src/components/estate-popular-houses.jsx
@@ -7,13 +7,29 @@ export default class EstatePopularItems extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            shown_items: houses.slice(0,3),
+            shown_text: "See More"
+        }
+        this.toggleHouses = this.toggleHouses.bind(this);
+    }
 
+    toggleHouses () {
+        if (this.state.shown_items.length === 3) {
+            this.setState({ 
+                shown_items: houses,
+                shown_text: "See Less"
+            })
+        } else {
+            this.setState({ 
+                shown_items: houses.slice(0,3),
+                shown_text: "See More"
+            })
         }
     }
 
     render() {
 
-        const shown_items = houses.slice(0,3);
+        const { shown_items, shown_text } = this.state;
 
         return (
             <div style={{ padding: '40px 0'}}>
@@ -22,11 +38,11 @@ export default class EstatePopularItems extends React.Component {
                         <h2 class="title" style={{ marginRight: '15px', color: '#393838' }}>Popular of the Week</h2>
                         <span style={{ marginBottom: '-5px', fontSize: '14px', color: '#393838', fontWeight: '500' }}>{houses.length} items</span>
                     </div>
-                    <div class="more" style={{ color: '#097FDE' }}>
-                        <Link to="" style={{ textDecoration: 'none', color: 'inherit'}}>See More</Link>
+                    <div class="more" style={{ color: '#097FDE', cursor: 'pointer' }} onClick={() => this.toggleHouses()}>
+                        {shown_text}
                     </div>
                 </div>
-                <div class="items" style={{ display: 'flex' }}>
+                <div class="items" style={{ display: 'flex', flexWrap: 'wrap' }}>
                     {shown_items.map(item => <EstatePopularEachItem item={item} />)}
                 </div>
             </div>
