4ea1c7d643b725c680dd60378a4ea41fed243f7b
Your Name <you@example.com>
2022-08-03 09:13:21 -0700

    Added icon for toggle

--- src/views/dark-mode.jsx
+++ src/views/dark-mode.jsx
@@ -16,7 +16,11 @@ export default function DarkMode () {
                     <h3>Today is the best day</h3>
                     <div className="mode-switch" onChange={() => toggleMode()}>
                         <input type="checkbox" className="mode-input"/>
-                        <div className="toggle"></div>
+                        <div className="toggle">
+                            { dark 
+                            ? <img src="https://www.freeiconspng.com/uploads/moon-icon-32.png" style={{ width: '30px' }}/>
+                            : <img src="https://www.freeiconspng.com/thumbs/sun-icon/sun-icon-31.png" style={{ width: '25px' }}/> }
+                        </div>
                     </div>                    
                 </div>
                 <div className="mode-text">
