cb5db81771554fe566e0d78f888c0e95b47fbd9a
Your Name <you@example.com>
2022-08-10 12:58:56 -0700

    Displayed RGB values

--- src/views/color-picker.jsx
+++ src/views/color-picker.jsx
@@ -4,12 +4,23 @@ export default class ColorPicker extends React.Component {
     constructor (props) {
         super (props)
         this.state = {
-
+            r: 0, 
+            g: 0,
+            b: 0
         }
+    }
+
+    updateRGB(color) {
+        this.setState({
+            r: color.r,
+            g: color.g,
+            b: color.b
+        })
     }
 
     render () {
 
+        const { r, g, b } = this.state;
         const colors = [];
 
         for (let j = 0; j < 30; j++) {
@@ -17,8 +28,8 @@ export default class ColorPicker extends React.Component {
             for (let i = 0; i < 30; i++) {
                 row.push({
                     r: 255 - i*8,
-                    g: (j-i)*9,
-                    b: (j-i)*9
+                    g: 0 + j*8 - i*8,
+                    b: 0 + j*8 - i*8
                 })
             }
             colors.push(row)
@@ -27,14 +38,24 @@ export default class ColorPicker extends React.Component {
         return (
             <div className="picker-container" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh'}}>
                 <div className="picker-wrapper" >
+                    <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '10px' }}>
+                        <div>R: <span>{r}</span></div>
+                        <div>G: <span>{g >=0 ? g : 0}</span></div>
+                        <div>B: <span>{b >=0 ? b : 0}</span></div>
+                    </div>
+                    <div className="colors">
                     {colors.map(colorRow => 
                         <div style={{ display: 'flex' }}>
                         {colorRow.map(color => 
-                            <div style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`, width: '20px', height: '20px'}}>
+                            <div 
+                                style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`, width: '20px', height: '20px'}}
+                                onClick={() => this.updateRGB(color)}
+                            >
                             </div>
                         )}
                         </div>
                     )}
+                    </div>
                 </div>
             </div>
         )
