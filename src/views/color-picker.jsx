85f29dfb92b6a48e851dbad842197e3302a0e6a9
Your Name <you@example.com>
2022-08-11 09:28:17 -0700

    Added more colors

--- src/views/color-picker.jsx
+++ src/views/color-picker.jsx
@@ -6,7 +6,17 @@ export default class ColorPicker extends React.Component {
         this.state = {
             r: 0, 
             g: 0,
-            b: 0
+            b: 0,
+            color_options: [
+                { r: 255, g: 0, b: 0},
+                { r: 0, g: 255, b: 0},
+                { r: 0, g: 0, b: 255},
+            ],
+            selected_color: {
+                r: 255,
+                g: 0,
+                b: 0
+            }
         }
     }
 
@@ -18,19 +28,37 @@ export default class ColorPicker extends React.Component {
         })
     }
 
+    updateSelectedColor(color) {
+        this.setState({ selected_color: color })
+    }
+
     render () {
 
-        const { r, g, b } = this.state;
+        const { r, g, b, color_options, selected_color } = this.state;
         const colors = [];
 
         for (let j = 0; j < 30; j++) {
             let row = []
             for (let i = 0; i < 30; i++) {
-                row.push({
-                    r: 255 - i*8,
-                    g: 0 + j*8 - i*8,
-                    b: 0 + j*8 - i*8
-                })
+                if (selected_color.r === 255) {
+                    row.push({
+                        r: 255 - i*8,
+                        g: 0 + j*8 - i*8,
+                        b: 0 + j*8 - i*8
+                    })
+                } else if (selected_color.g === 255) {
+                    row.push({
+                        r: 0 + j*8 - i*8,
+                        g: 255 - i*8,
+                        b: 0 + j*8 - i*8
+                    })
+                } else if (selected_color.b === 255) {
+                    row.push({
+                        r: 0 + j*8 - i*8,
+                        g: 0 + j*8 - i*8,
+                        b: 255 - i*8
+                    })
+                }
             }
             colors.push(row)
         }
@@ -43,18 +71,29 @@ export default class ColorPicker extends React.Component {
                         <div>G: <span>{g >=0 ? g : 0}</span></div>
                         <div>B: <span>{b >=0 ? b : 0}</span></div>
                     </div>
-                    <div className="colors">
-                    {colors.map(colorRow => 
-                        <div style={{ display: 'flex' }}>
-                        {colorRow.map(color => 
-                            <div 
-                                style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`, width: '20px', height: '20px'}}
-                                onClick={() => this.updateRGB(color)}
-                            >
+                    <div className="colors" style={{ display: 'flex' }}>
+                        <div className="colors-options" style={{ marginRight: '20px' }}>
+                            {color_options.map(color => 
+                                <div 
+                                    style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`, width: '20px', height: '20px', cursor: 'pointer' }}
+                                    onClick={() => this.updateSelectedColor(color)}
+                                >
+                                </div>
+                            )}
+                        </div>
+                        <div className="colors-display">
+                        {colors.map(colorRow => 
+                            <div style={{ display: 'flex' }}>
+                            {colorRow.map(color => 
+                                <div 
+                                    style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`, width: '20px', height: '20px'}}
+                                    onClick={() => this.updateRGB(color)}
+                                >
+                                </div>
+                            )}
                             </div>
                         )}
                         </div>
-                    )}
                     </div>
                 </div>
             </div>
